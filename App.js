b5c23e33aafb4ad295f568b885ee2cec8a8fd9bd
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-15 16:21:05 -0600

    CONTROL FLOW if/else Statements

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-// Initialize constant myAge
-const myAge = 42;
-// Initialize variable earlyYears
-let earlyYears = 2;
-
-earlyYears *= 10.5;
-
-let laterYears = myAge - 2;
-
-laterYears *= 4;
-
-console.log(earlyYears);
-console.log(laterYears);
-
-// Declares myAgeInDogYears and adds both earlyYears and laterYears
-let myAgeInDogYears = earlyYears + laterYears;
-
-'Alexis Zambrano'.toLowerCase();
-
-console.log(`My name is ${'Alexis Zambrano'.toLowerCase()}. I am ${myAgeInDogYears} years old in dog years`)
-
+let isSoccerFan = true;
+if (isSoccerFan === true) {
+    console.log('Goal!');
+} else {
+    console.log('No goal!');
+}
