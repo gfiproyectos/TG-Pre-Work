c6dd41aa7cf63ab70cfd6acd1908f3cba904e2e1
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-15 15:43:31 -0600

    LEARN JAVASCRIPT: INTRODUCTION Kelvin Weather

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
-console.log(Math.floor(Math.random() * 100));
-console.log(Math.ceil(43.8));
-console.log(Number.isInteger(2017));
+// Declares constant variable kelvin
+const kelvin = 280;
+// Declares constant variable celsius and subtracts 273 from kelvin
+const celsius = kelvin - 273;
+// Declares variable fahrenheit and calculates based in celsius
+let fahrenheit = celsius * (9 / 5) + 32;
+// Use of the Math.floor method to round down the fahrenheit value
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`)
