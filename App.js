4fac52501038bcbb06778cd17cb14a0ee4a25d78
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-18 12:11:35 -0600

    LEARN JAVASCRIPT: CONTROL FLOW Race Day

--- App.js
+++ App.js
@@ -1,54 +1,21 @@
-let userName = '';
+let raceNumber = Math.floor(Math.random() * 1000);
 
-userName ?
-  console.log(`Hello ${userName}`) :
-  console.log("Hello");
+const registeredEarly = true;
+const age = 18;
 
-const userQuestion = 'Who is Alexa';
+if (age > 18 && registeredEarly) { 
+   raceNumber += 1000;
+}
 
-console.log(`The user asked: ${userQuestion}`);
-
-const randomNumber = Math.floor(Math.random() * 8);
-
-console.log(`The randomNumber: ${randomNumber}`);
-
-let eightBall = '';
-
-switch (randomNumber) {
-        
-        case 0:
-          eightBall = 'It is certain';
-          break;
-    
-        case 1:
-          eightBall = 'It is decidedly so';
-          break;
-    
-        case 2:
-          eightBall = 'Reply hazy try again';
-          break;
-    
-        case 3:
-          eightBall = 'Cannot predict now';
-          break;
-    
-        case 4:
-          eightBall = 'Do not count on it';
-          break;
-    
-        case 5:
-          eightBall = 'My sources say no';
-          break;
-    
-        case 6:
-          eightBall = 'Outlook not so good';
-          break;
-    
-        case 7:
-          eightBall = 'Signs point to yes';
-          break;
-    
-        }
-
-console.log(`The Magic Eight Ball answered: ${eightBall}`);
-
+if (age > 18 && registeredEarly) { 
+   console.log(`Runner will race at 9:30 am and 
+Number will be ${raceNumber} `);
+} else if (age > 18 && !registeredEarly) {
+  console.log(`Runner will race at 11:30 am and 
+Number will be ${raceNumber} `);
+} else if (age < 18 ) {
+  console.log(`Runner will race at 12:30 am and 
+Number will be ${raceNumber} `);
+} else {
+  console.log('Runner should see the registration desk');
+}
