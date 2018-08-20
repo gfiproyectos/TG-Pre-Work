a6e94541cde0b0dbe3241c9fdc2f85d645747163
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-20 12:36:42 -0600

    FUNCTIONS Return

---
+++ Main.js
@@ -0,0 +1,16 @@
+let orderCount = 0;
+
+const takeOrder = (topping, crustType) => {
+  orderCount += 1;
+  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
+};
+
+ const getSubTotal = (itemCount) => {
+    return itemCount * 7.5;
+ };
+
+takeOrder('mushroom', 'thin crust');
+takeOrder('spinach', 'whole wheat');
+takeOrder('pepperoni', 'brooklyn style');
+
+console.log(getSubTotal(orderCount));
