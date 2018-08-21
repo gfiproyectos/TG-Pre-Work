ae50fde4c9d88ba7ea5aaa4f1fe7ba79c53276fc
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-21 15:21:34 -0600

    ARRAYS Create an array

--- Main.js
+++ Main.js
@@ -1,24 +1,3 @@
-let orderCount = 0;
+let newYearsResolutions = ['Learn AWS' , 'Get a 50$/hr job' , 'Be Happy...'];
 
-const takeOrder = (topping, crustType) => {
-  orderCount++;
-  console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-};
-
-takeOrder('mushroom', 'thin crust');
-takeOrder('spinach', 'whole wheat');
-takeOrder('pepperoni', 'brooklyn style');
-
-const getSubTotal = (itemCount) => {
-  return itemCount * 7.5;
-};
-
-const getTax = (itemCount) => {
-  return getSubTotal(itemCount) * 0.06;
-};
-
-const getTotal = (itemCount) => {
-  return getSubTotal(itemCount) + getTax(itemCount);
-};
-
-console.log(getTotal(orderCount));
+console.log(newYearsResolutions);
