9e8ec962d15e77348f7522818cbc1ca7624b13f4
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-22 13:27:50 -0600

    LEARN JAVASCRIPT: ITERATORS Mini Linter

--- Main.js
+++ Main.js
@@ -1,15 +1,31 @@
-let cities = ['Nashville', 'Charlotte', 'Asheville', 'Austin', 'Boulder'];
+let story = 'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
 
-let nums = [1, 50, 75, 200, 350, 525, 1000];
+let overusedWords = ['really', 'very', 'basically'];
 
-//  Choose a method that will return undefined
-cities.forEach(city => console.log('Have you visited ' + city + '?'));
+let unnecessaryWords = ['extremely', 'literally', 'actually' ];
 
-// Choose a method that will return a new array
-let longCities = cities.filter(city => city.length > 7);
+let storyWords = story.split(' ');
 
-// Choose a method that will return a new array
-let smallerNums = nums.map(num => num - 5);
+let betterWords = storyWords.filter(function(word) {
+  return unnecessaryWords.includes(word) === false;
+});
 
-// Choose a method that will return a boolean value
-nums.every(num => num < 0);
+let howManyOver = 0;
+storyWords.forEach(function(word) {
+    if (overusedWords.includes(word)) {
+      howManyOver++;
+    }
+  });
+
+let sentences = 0;
+storyWords.forEach(word => {
+  if (word[word.length-1] === '.' || word[word.length-1] === '!') {
+    sentences+=1;
+  }
+});
+
+console.log('There are ' + storyWords.length + ' Words');
+console.log('There are ' + howManyOver + ' Overused Words');
+console.log('There are ' + sentences + ' sentences');
+
+console.log(betterWords.join(' '));
