21eaf8107408d144730e0f56c59ce0e0653c06f6
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-24 17:46:55 -0600

    LEARN JAVASCRIPT: OBJECTS Team Stats

--- App.js
+++ App.js
@@ -1,82 +1,58 @@
-const menu = {
-
-    _courses : {
-      appetizers: [],
-      mains: [],
-      desserts: []
+const team = {
+    _players: [
+      {firstName: 'Eddie', 
+           lastName: 'Acosta', 
+           age: 44},
+      {firstName: 'Jorge', 
+           lastName: 'Rojas', 
+           age: 38},
+      {firstName: 'Luis', 
+           lastName: 'Vides', 
+           age: 36}
+    ],
+    _games: [{opponent: 'Broncos', teamPoints: 42, opponentPoints: 27},
+            {opponent: 'Bravos', teamPoints: 12, opponentPoints: 5},
+            {opponent: 'Bears', teamPoints: 3, opponentPoints: 8}],
+    
+    get players () {
+    
     },
-    get courses() {
-        return {appetizers: this._courses.appetizers,
-                mains: this._courses.mains,
-                desserts: this._courses.desserts}
-      },
+    get games () {
     
-    set appetizers(appetizerIn) {
+    },
   
-      },
-    get appetizers() {
+    addPlayer  (playerFirstName, playerLastName, playerAge)  {
+                
+          let player = {
+            firstName: playerFirstName,
+            lastName: playerLastName,
+            age: playerAge
+          }
+          
+          this._players.push(player);
+          
+        },  
   
-      },
-    set mains(mainIn) {
-  
-      },
-    get mains() {
-  
-      },
-    set desserts(dessertIn) {
-  
-      },
-    get desserts () {
-  
-      },
+    addGame  (gameOpponent, gameTeamPoints, gameOpponentPoints)  {
+                
+          let game = {
+            opponent: gameOpponent,
+            teamPoints: gameTeamPoints,
+            opponentPoints: gameOpponentPoints
+          }
+          
+          this._games.push(game);
+          
+        }  
     
-      addDishToCourse (courseName, dishName, dishPrice)  {
-              
-        let dish = {
-          name: dishName,
-          price: dishPrice
-        }
-        
-        if (courseName === 'appetizers') {
-            this._courses.appetizers.push(dish);
-        }  else if (courseName === 'mains') {
-            this._courses.mains.push(dish);
-        } else if (courseName === 'desserts'){
-            this._courses.desserts.push(dish);           
-        };
-        
-      },
-    
-      getRandomDishFromCourse (courseName) {
-        const dishes = this._courses[courseName];
-        const randomIndex = Math.floor(Math.random() * dishes.length);
-  
-        return dishes[randomIndex]
-        },
-      
-      generateRandomMeal: function() {
-      const appetizer = this.getRandomDishFromCourse('appetizers');
-      const mains = this.getRandomDishFromCourse('mains');
-      const desserts  = this.getRandomDishFromCourse('desserts');
-        
-      const totalPrice = appetizer.price + mains.price + desserts.price 
-  
-      return `Your meal is ${appetizer.name}, ${mains.name}, ${desserts.name} The price is $${totalPrice}.`;
-    }
-    
-  }
-  
-  menu.addDishToCourse('appetizers', 'Caesar Salad', 4.25);
-  menu.addDishToCourse('appetizers', 'Mozzarella Stix', 5.25);
-  menu.addDishToCourse('appetizers', 'Onion Soup', 6.25);
-  menu.addDishToCourse('mains', 'Chicken', 9.25);
-  menu.addDishToCourse('mains', 'Steak', 15.25);
-  menu.addDishToCourse('mains', 'Filet Mignon', 16.25);
-  menu.addDishToCourse('desserts', 'Churros', 4.25);
-  menu.addDishToCourse('desserts', 'Cinnamon Stix', 5.25);
-  menu.addDishToCourse('desserts', 'Cheese Cake', 6.25);
+  };
   
   
-  let meal = menu.generateRandomMeal();
-  console.log(meal);
   
+  team.addPlayer("Steph", "Curry", 28);
+  team.addGame("Rays", 5, 6);
+  team.addGame("Nets", 88, 55);
+  team.addGame("Rams", 44, 21);
+  
+  console.log(team._players);
+  console.log(team._games);  
