17c3c59f68568c12bfa7d3aa372575789a5cd3dd
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-25 09:37:11 -0600

    CLASSES Methods

--- Main.js
+++ Main.js
@@ -1,26 +1,27 @@
-let person = {
-    _name: 'Lu Xun',
-    _age: 137,
-    
-    set age(ageIn) {
-      if (typeof ageIn === 'number') {
-        this._age = ageIn;
-      }
-      else {
-        console.log('Invalid input');
-        return 'Invalid input';
-      }
-    },
-    
-    get age() {
-      console.log(`${this._name} is ${this._age} years old.`);
-      return this._age;
+class Surgeon {
+    constructor(name, department) {
+      this._name = name;
+      this._department = department;
+      this._remainingVacationDays  = 20;
     }
+    
+    get name() {
+      return this._name;
+    }
+    
+    get department() {
+      return this._department;
+    }
+    
+    get remainingVacationDays() {
+      return this._remainingVacationDays;
+    }
+    
+    takeVacationDays (daysOff) {
+      this._remainingVacationDays = this._remainingVacationDays - daysOff;
+    }
+    
+  }
   
-  };
-  
-  
-  person.age = 'Thirty-nine';
-  person.age = 39;
-  
-  console.log(person.age);
+  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
+  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
