ecad178fa76dbd559f981fa40f88555985d360fe
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-25 10:10:01 -0600

    CLASSES Inheritance V

--- Main.js
+++ Main.js
@@ -1,27 +1,40 @@
-class Surgeon {
-    constructor(name, department) {
+class HospitalEmployee {
+    constructor(name) {
       this._name = name;
-      this._department = department;
-      this._remainingVacationDays  = 20;
+      this._remainingVacationDays = 20;
     }
     
     get name() {
       return this._name;
     }
     
-    get department() {
-      return this._department;
-    }
-    
     get remainingVacationDays() {
       return this._remainingVacationDays;
     }
     
-    takeVacationDays (daysOff) {
-      this._remainingVacationDays = this._remainingVacationDays - daysOff;
+    takeVacationDays(daysOff) {
+      this._remainingVacationDays -= daysOff;
+    }
+  }
+  
+  class Nurse extends HospitalEmployee {
+    constructor(name, certifications) {
+      super(name);
+      this._certifications = certifications;
+    } 
+    
+    get certifications() {
+      return this._certifications;
+    }
+    
+    addCertification (newCertification) {
+      this._certifications.push(newCertification);
     }
     
   }
   
-  const surgeonCurry = new Surgeon('Curry', 'Cardiovascular');
-  const surgeonDurant = new Surgeon('Durant', 'Orthopedics');
+  const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+  nurseOlynyk.takeVacationDays(5);
+  nurseOlynyk.addCertification('Genetics');
+  
+  console.log(nurseOlynyk.certifications);
