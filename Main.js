ece9b5ce80c85461f81ebcf451c9d429885c775a
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-26 00:27:32 -0600

    REQUESTS II fetch() GET Requests IV

--- Main.js
+++ Main.js
@@ -2,7 +2,6 @@
 const url = 'https://api.datamuse.com/words';
 const queryParams = '?sl=';
 
-
 // Selects page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
@@ -11,21 +10,21 @@ const responseField = document.querySelector('#responseField');
 // AJAX function
 const getSuggestions = () => {
   const wordQuery = inputField.value;
-  const endpoint = url + queryParams + wordQuery;
+  const endpoint = `${url}${queryParams}${wordQuery}`;
   
   fetch(endpoint).then(response => {
     if (response.ok) {
-       return response.json()
+      return response.json();
     }
     throw new Error('Request failed!');
   }, networkError => {
-    console.log(networkError.message);
-    }
-    )                   
-  
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+     renderResponse(jsonResponse)
+  } );
 }
-  
-  // Clears previous results and display results to webpage
+
+// Clears previous results and display results to webpage
 const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
