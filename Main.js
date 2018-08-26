b97c006df079f403769888f2883d9326866c7cd0
AZ-PC\manager <gfiproyectos@gmail.com>
2018-08-26 01:05:42 -0600

    REQUESTS II async GET Requests III

--- Main.js
+++ Main.js
@@ -1,43 +1,39 @@
 // Information to reach API
-const apiKey = '<Your API Key>';
-const url = 'https://api.rebrandly.com/v1/links';
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
 
-// Some page elements
+// Selecting page elements
 const inputField = document.querySelector('#input');
-const shortenButton = document.querySelector('#shorten');
+const submit = document.querySelector('#submit');
 const responseField = document.querySelector('#responseField');
 
-// AJAX functions
-const shortenUrl = () => {
-  const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten})
+// AJAX function
+// Code goes here
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = url + queryParams + wordQuery;
   
-	fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'apikey': apiKey
-    },
-    body: data
-  }).then(response => {
-    if(response.ok){
-	    return response.json();  
-    }
-	  throw new Error('Request failed!');
-  }, networkError => {
-  console.log(networkError.message);
-  }).then(jsonResponse => {
-    renderResponse(jsonResponse);
-})
+  try {
+   const response = await fetch(endpoint);
+   if (response.ok) {
+      const jsonResponse = await   response.json();
+      renderResponse(jsonResponse);
+    } 
+    
+  } catch (error)  {
+    console.log(error)
+  }
+  
 }
 
-// Clear page and call AJAX functions
-const displayShortUrl = (event) => {
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
     responseField.removeChild(responseField.firstChild)
   }
-  shortenUrl();
+  getSuggestions();
 }
 
-shortenButton.addEventListener('click', displayShortUrl);
+submit.addEventListener('click', displaySuggestions);
